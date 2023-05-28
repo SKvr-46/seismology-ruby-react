5cef56b2775ac9b64ad5c9fd5601aa105379c972
SKvr-46 <koeich4@gmail.com>
2023-05-28 13:09:41 +0900

    add image

--- app/python/WavenumberCalculation.py
+++ app/python/WavenumberCalculation.py
@@ -1,10 +1,11 @@
 import sys
 import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
-row = int(sys.argv[1])
-column = int(sys.argv[2])
+row = int(sys.argv[1]) 
+column = int(sys.argv[2]) 
+nx = int(sys.argv[2])
+ny = int(sys.argv[1])
 dx = int(sys.argv[3])
 dy = int(sys.argv[4])
 
@@ -20,10 +21,6 @@ for i in range(row):
 
 # 入力されたすべりのデータ
 slip = np.array(two_d_array)
-print(slip)
-
-nx = column
-ny = row
 
 
 # #波数スペクトル行列の行（列）で。波数0が何番目なのかを探索する関数（必須ではない）
@@ -43,8 +40,8 @@ ky = 2*np.pi*np.fft.fftfreq(slip.shape[0], d=dx) #データ点数10個
 modified_ky = np.fft.fftshift(ky)    #波数の列を小さい順にする
 ky_spec =np.abs(np.fft.fftshift(np.fft.fft2(slip))) #振幅
 
+print("kx:", len(kx), "modified_kx:", len(modified_kx), "kx_spec:", len(kx_spec),"kx_spec shape:", kx_spec.shape,"nx", nx, "ny",ny)
 
-# #出力する
 fig, (ax1, ax2,ax3) = plt.subplots(1,3, figsize=(15, 5))
 ax1.plot(modified_kx, kx_spec[judge(ny),:], 'b.-')  
 ax1.set_xlabel('kx')
@@ -59,27 +56,34 @@ ax2.set_title('Wavenumber spectrum along y-axis')
 ax2.set_xlim(0, max(modified_ky))
 
 
-# 2次元波数スペクトル（の絶対値の自然対数の分布）を描画する
+# # 2次元波数スペクトル（の絶対値の自然対数の分布）を描画する
 im = ax3.imshow(np.log10(np.abs(np.fft.fftshift(np.fft.fft2(slip)))), cmap='jet', origin="lower",extent=(min(kx), max(kx), min(ky), max(ky)))
 ax3.set_xlabel('kx')
 ax3.set_ylabel('ky')
 ax3.set_title('2D Wavenumber spectrum')
 fig.colorbar(im, ax=ax3)
 im.autoscale()
-plt.show()
+
+fig.savefig('figure.png')
+plt.close(fig)
+
+
 
 #すべり分布の出力
+fig, ax = plt.subplots(figsize=(5, 5))
 plt.imshow(slip, cmap='jet')
 plt.colorbar()
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title("slip")
-plt.show()
+fig.savefig('slip_plot.png')
+plt.close(fig)
 
 
 #波数0で1に規格化された縦方向の波数スペクトルと、横方向の波数スペクトルの出力
 #kx_specの規格化：kx=0の列で全体を割る。
 #こうすることで、どのkyの場合でも、始まりが1になる。
+fig, ax = plt.subplots(figsize=(8, 6))
 kx_spec_normalized = kx_spec / kx_spec[:,judge(nx)][:, np.newaxis] 
 plt.plot(modified_kx,kx_spec_normalized[judge(ny),:], 'b.-')  
 
@@ -91,4 +95,5 @@ plt.xlabel('wavenumber')
 plt.ylabel('amplitude')
 plt.title('Normalized Wavenumber spectrum ')
 plt.xlim(0, max(max(modified_ky), max(modified_kx)))
-plt.show()
+fig.savefig('spectrum_plot.png')
+plt.close(fig)

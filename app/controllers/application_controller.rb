0d47bccb98f237658a9671e29a43dd528a8a056a
SKvr-46 <koeich4@gmail.com>
2023-05-30 21:24:00 +0900

    add User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+    def page 
+        render html: "this page is not allowed"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   root to: 'site#index'
   post '/wavenumber_calculation', to: 'calculations#wavenumber_calculation'
 end
